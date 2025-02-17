e4597d34a95217a4260a723eb4da1fac0080cf3d
Maham <kanwalmhsm@gmail.com>
2025-02-17 23:09:18 +0500

    made a server

---
+++ app.mjs
@@ -0,0 +1,21 @@
+import express from "express"
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/weather', (req, res) => {
+    res.json({ user: 'maham kanwal' })
+  })
+  app.get('/weather/:cityName', (req, res) => {
+    res.send(req.params)
+  })
+  app.get('/weather/:cityName/:side', (req, res) => {
+    res.send(req.params)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
