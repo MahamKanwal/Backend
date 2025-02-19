2591a6886766bcb693e744650c95e4e5f10c6d7c
Maham <kanwalmhsm@gmail.com>
2025-02-19 14:47:14 +0500

    make a folder

--- app.mjs
+++ app.mjs
@@ -2,6 +2,9 @@ import express from "express"
 const app = express()
 const port = process.env.PORT || 3000
 
+const path = require('path')
+app.use('/', express.static(path.join(__dirname, './web/dist')))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -19,3 +22,35 @@ app.get('/weather', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// import express from "express"
+// import path from "path"
+// const app = express()
+// const port = process.env.PORT ||  8080
+
+// const __dirname = path.resolve()
+
+// app.use('/', express.static(path.join(__dirname, './React/dist')))
+
+
+
+// app.get('/', (req, res) => {
+//   res.send("get  chlrha ha")
+// })
+
+// app.get('/weather', (req, res) => {
+//   res.json({ user: 'tobi' })
+// })
+// app.get('/weather/:cityName', (req, res) => {
+//   res.send(req.params)
+// })
+// app.get('/weather/:cityName/:side', (req, res) => {
+//   res.send(req.params)
+// })
+
+
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
