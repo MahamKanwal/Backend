09d4279d03c0d5dc96bdf7ae9c14e279e2d25c3f
Maham <kanwalmhsm@gmail.com>
2025-02-19 14:57:13 +0500

    some changes

--- app.mjs
+++ app.mjs
@@ -1,8 +1,9 @@
 import express from "express"
+import path from "path"
 const app = express()
 const port = process.env.PORT || 3000
 
-const path = require('path')
+const __dirname = path.resolve()
 app.use('/', express.static(path.join(__dirname, './web/dist')))
 
 app.get('/', (req, res) => {
